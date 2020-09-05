c5abbeacec94eea807a652d62666008012595dce
Numerix <42088870+Numerix76@users.noreply.github.com>
2020-09-06 00:08:19 +0200

    Update numerix_ano_cmd.lua

--- lua/autorun/server/numerix_ano_cmd.lua
+++ lua/autorun/server/numerix_ano_cmd.lua
@@ -7,7 +7,7 @@ hook.Add("PlayerSay", "PlayerSay:AnoCommande", function(ply, txt)
 	if string.sub(txt, 1, string.len("/ano")) == "/ano" then
 		local message = string.Explode( "/ano ", txt )[2]
 
-		for k,v in pairs(player.GetAll()) do 
+		for k,v in ipairs(player.GetAll()) do 
 			if AllowedGroup[v:GetNWString("usergroup")] then   
 				DarkRP.talkToPerson(v, Color(0, 0, 0, 255),  "[Anonyme] ".. ply:Nick() .. " " , Color(255, 255, 0, 255), message, ply)
 			else
